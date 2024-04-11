46287a528fdea89107486a37386189427c91be08
antoplati <aplati@udesa.edu.ar>
2024-04-11 12:51:02 -0300

    empezando detalle pelicula

--- src/screens/DetallePelicula.js
+++ src/screens/DetallePelicula.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 class DetallePelicula extends Component {
+    constructor(props){
+        super(props)
+    }
   render() {
+    let id = this.props.match.params.id
     return (
-      <div>DetallePelicula</div>
+      <div>Detail of {id}</div>
     )
   }
 }
 
-export default DetallePelicula
+export default DetallePelicula;
+
